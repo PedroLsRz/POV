a7b68b2a4080d088d28ffe657a03feb2849ec519
PedroLsRz <pedrodbr955@gmail.com>
2023-09-28 19:42:27 -0300

    refactor: ajuste do css

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./css/cadastro.css" rel="stylesheet">
+    <link href="./estilos/cadastro.css" type="text/css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./css/login.css" rel="stylesheet">
+    <link href="./estilos/login.css" rel="stylesheet">
   <!--  <link href="./css/bootstrap.min.css" rel="stylesheet">  -->
     <title>PoV página de login</title>
 </head>
