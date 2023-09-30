fd41eb76bf2755bb2599891ee7d6048bd7520b95
Jakeline Silva <114974859+Liidys@users.noreply.github.com>
2023-09-30 11:42:44 -0300

    refactor: atualização

--- logout.php
+++ logout.php
@@ -1,17 +1,8 @@
 <?php
-<<<<<<< HEAD
-
-
-=======
 #Iniciar uma sessão
->>>>>>> origin/main
 session_start();
 #Terminar a sessão iniciada e redirecionar o usuário para a página declarada
 session_destroy();
 header('Location: login.php');
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/main
 ?>
