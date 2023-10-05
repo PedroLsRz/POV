fd8db16794b82cdae175e7219098c8af4ce05f21
PedroLsRz <pedrodbr955@gmail.com>
2023-10-05 15:24:31 -0300

    refactor: alteração na página de logina

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link href="./estilos/styleslogin.css" rel="stylesheet">
+    
   <!--  <link href="./css/bootstrap.min.css" rel="stylesheet">  -->
     <title>PoV-Página de login</title>
 </head>
@@ -18,12 +19,15 @@
                         <!-- Formuláro de cadastro-->
     <form class= "forms" action="" id="form-login" method = "POST" >
         <h1 class = "tituloform">LOGIN</h1>
-            <h1 class ="titulogin">RA: <br>
-                <input class ="textologin" type = "text" name = "login" required> 
+            <h1 class ="titulogin">RA<br><br>
+            <i></i>
+                <input type = "text" name = "login" required> 
             </h1>
-        <h1 class ="titulosenha">Senha:</h1>
-            <input class= "textosenha" type = "password" name="senha" required>
+        <h1 class ="titulosenha">Senha<br><br>
+            <i></i>
+            <input type = "password" name="senha" required>
                 <br>
+        </h1>
             <button type= "submit" class= "botao" name= "entrar">
                 ENTRAR
         </button>
