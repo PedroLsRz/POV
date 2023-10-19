0383f60da1cdf2c37fb346d2067cf3b5e6cb91db
Jakeline Silva <114974859+Liidys@users.noreply.github.com>
2023-10-18 22:38:16 -0300

    feat:" criação da página de exlusão  admin"

--- admin.php
+++ admin.php
@@ -34,7 +34,7 @@ if ($_SESSION['logado'] <> true) {
                 <div class="menu">
                  <h1 class="usuario">Usuário</h1>
                  <button type="submit" class="botao" name="cadastro" onclick="window.location.href='./cadastroadmin.php'">Criar Usuário</button>
-                 <button type="submit" class="botao" name="excluir">Excluir Usuário</button>
+                 <button type="submit" class="botao" name="excluir" onclick="window.location.href='./excluirusuario.php'">Excluir Usuário</button>
                  <button type="submit" class="botao" name="edicao">Editar dados <br>de um Usuário</button>
                  <h1 class="aplicacao">Aplicação</h1>
                  <button type="submit" class="botao" name="editar">Editar salas</button>
---
+++ excluirusuario.php
@@ -0,0 +1,68 @@
+<?php
+require_once "bancoexcluirusuario.php";
+#Iniciar sessão
+session_start();
+#Se a sessão LOGADO for diferente de VERDADEIRA, redirecione para a página indicada
+if ($_SESSION['logado'] <> true) {
+  header('Location: login.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./estilos/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <link href="./estilos/stylesmenu.css" rel="stylesheet">
+    <title>PoV</title>
+</head>
+<body>
+    <div class="fundo">
+
+                <!-- Barra de navegação-->  
+            <nav class="navbar">
+
+                <img class= "logo" src="img/logoNavBar.png" > 
+                <!-- Cabeçalho da barra de navegação printando o nome do 
+                usuário registrado no banco de dados ao se logar com sucesso-->
+                <a class="saudacao">Escolha o usuario<br>que deseja excluir!</a>
+                <!-- Link para encerrar a sessão montada no arquivo declarado abaixo-->
+
+                <a class = "sair" href="logout.php">Sair</a>
+
+            </nav>
+            
+        
+        <div class= "format">
+            <input class="inputsalas" type="text">
+            <ul class="listasalas">
+            <?php
+            usuarios();
+            ?>
+            </ul>
+        </div>
+
+        <script>
+            let inputElement = document.querySelector("input")
+            let listElement = document.querySelector("ul")
+            let itemElement = listElement.querySelectorAll("h")
+
+            inputElement.addEventListener("input", (e) => {
+            let inputed = e.target.value.toLowerCase()
+            itemElement.forEach((h) => {
+                let text = h.textContent.toLowerCase()
+                if(text.includes(inputed)){
+                h.style.display = "block"
+                }else{
+                h.style.display = "none"
+                }
+                
+            })
+            })
+        </script>
+        
+    </div>
+</body>
+</html>
