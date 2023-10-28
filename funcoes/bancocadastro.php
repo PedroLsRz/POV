8ec0b6ce21333d352166167dd78b01f9ed61a09d
PedroLsRz <pedrodbr955@gmail.com>
2023-10-28 19:56:58 -0300

    feat: adição do "confirmar senha"

--- funcoes/bancocadastro.php
+++ funcoes/bancocadastro.php
@@ -7,17 +7,24 @@ include ('conexao.php');
 $nome= $_POST ['nometexto'];
 $user= $_POST ['logintexto'];
 $senha= $_POST ['senhatexto'];
+$senha2= $_POST ['senhatexto2'];
+
 
 #fazendo uma variável que recebe o SELECT
-$select = "INSERT into usuarios (nome, login, senha,tipo) values ('$nome', '$user', '$senha', 0)";
+$select = "INSERT into usuarios (nome, login, senha, senha2 ,tipo) values ('$nome', '$user', '$senha', '$senha2', 0)";
 
-try{
-#executa a $conexao e o $select dentro dela
-$query = mysqli_query($conexao, $select);
-    echo '<div class="sucessocadastro">Cadastro realizado com sucesso!</div>';
-}catch(Exception $e){
-    echo '<div class="errocadastro">RA já cadastrado!</div>';
+if($senha == $senha2) { 
+     
+    try{
+    #executa a $conexao e o $select dentro dela
+    $query = mysqli_query($conexao, $select);
+        echo '<div class="sucessocadastro">Cadastro realizado com sucesso!</div>';
+    }catch(Exception $e){
+        echo '<div class="errocadastro">RA já cadastrado!</div>';
+    }
+}elseif ($senha <> $senha2) {
+    echo '<div class="errocadastro">As senhas não coincidem!</div>';
 }
-}
-
+    
+} 
 ?>
