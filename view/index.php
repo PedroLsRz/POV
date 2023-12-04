65be38db2659a89ea6cbc9dbac0da42f4c418457
PedroLsRz <pedrodbr955@gmail.com>
2023-12-04 08:51:18 -0300

    feat: index.php

---
+++ view/index.php
@@ -0,0 +1,23 @@
+<?php
+#Iniciar sessão
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
+    <link href="../css/inicio.css" rel="stylesheet">
+    <title>PoV - Início</title>
+</head>
+<body>
+    <div class="fundo">
+        <div class= "format">
+            <img class= "bemvindo" src="../img/inicio.png" >
+            <img class= "botaoiniciar" onclick='location.href="inicio.php"' src="../img/botaoiniciar.png" >
+        </div>
+    </div>
+</body>
+</html>
